782656dece1198aef8c667d3f9b65323b2561462
pirate du bato <anonyme201504@gmail.com>
2024-08-19 23:54:31 +0200

    geofindme

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -10,6 +10,36 @@ declare var Celestial:any;
 export class Tab1Page implements OnInit {
 
   constructor() {}
+  geoFindMe() {
+  //const status = document.querySelector("#status");
+  //const mapLink = document.querySelector("#map-link");
+
+  //mapLink.href = "";
+  //mapLink.textContent = "";
+
+  function success(position:any) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    //status.textContent = "";
+    //mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    //mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+    $("#lat").val(latitude);
+    $("#lon").val(longitude);
+  }
+
+  function error() {
+    //status.textContent = "Unable to retrieve your location";
+  }
+
+  if (!navigator.geolocation) {
+    //status.textContent = "Geolocation is not supported by your browser";
+  } else {
+    //status.textContent = "Locating…";
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+}
+
 
   ngOnInit(){
 	   function reload_js(src:any) {
@@ -28,9 +58,10 @@ reload_css('/assets/lib/celestial.css')
 Celestial.display({
   location: true,
   projection: "airy",
-  datapath: "../data/",
+  datapath: "../assets/data/",
   planets:  { show: true }
 });
+this.geoFindMe()
 
   }
 
